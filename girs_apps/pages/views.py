2835386ed50684b3e16f6a1cb892626481384c47
techsolutionss <aulegabriel381@gmail.com>
2025-04-20 07:30:48 +0100

    changes

--- girs_apps/pages/views.py
+++ girs_apps/pages/views.py
@@ -31,7 +31,8 @@ class MapTemplateView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MapTemplateView, self).get_context_data(**kwargs)
         location = Location.objects.get(pk=self.request.GET.get("location_id"))
-        start_coordinates = [str(location.latitude), str(location.longitude)]
+        start_coordinates = [7.73375000, 8.52139000]
+        end_coordinates = [str(location.latitude), str(location.longitude)]
         user_latitude = self.request.GET.get("user_latitude", None)
         user_longitude = self.request.GET.get("user_longitude", None)
         # public_ip = requests.get("https://api.ipify.org")
@@ -39,27 +40,27 @@ class MapTemplateView(TemplateView):
         # response = requests.get("http://ip-api.com/json").json()
         # print(response)
         # if user_latitude and user_longitude:
-        end_coordinates = [7.73375000, 8.52139000]
         # test_coordinates = [41.850030, -87.650050]
         m = folium.Map(
                         location=start_coordinates,
-                        # control_scale=True,
-                        zoom_start=12,
+                        control_scale=True,
+                        zoom_start=13,
                         height='100%',
                        )
-        # folium.FeatureGroup(name="Icon collection", control=False).add_to(m)
+        folium.FeatureGroup(name="Icon collection", control=False).add_to(m)
 
         # different kind of layers
 
-        folium.Marker(location=start_coordinates, popup="Start Location" , icon=folium.Icon(color="green")).add_to(m)
-        folium.Marker(location=end_coordinates, popup="End Location" , icon=folium.Icon(color="red")).add_to(m)
-        # folium.PolyLine([start_coordinates, end_coordinates], color="blue").add_to(m)
-        # folium.LayerControl().add_to(m)
+        folium.Marker(location=start_coordinates, popup="Bsu markurdi" , icon=folium.Icon(color="red")).add_to(m)
+        folium.Marker(location=end_coordinates, popup=location.name , icon=folium.Icon(color="green")).add_to(m)
+        folium.PolyLine([start_coordinates, end_coordinates], color="blue").add_to(m)
+        folium.LayerControl().add_to(m)
 
         # get info about the location
         location_info = get_location_info(location.latitude, location.longitude)
         context["location_info"] = location_info
         context["map"] = m._repr_html_()
+        context["location"] =  location
         return context
     
 def get_locations_by_category(request):
